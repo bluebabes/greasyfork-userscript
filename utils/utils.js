4140add3cc9a84503a06267204392ec047e5d442
bluebabes <a308057848@163.com>
2022-04-07 17:07:32 +0800

    d

--- utils/utils.js
+++ utils/utils.js
@@ -3,10 +3,10 @@
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
     (global = global || self, factory(global.R = {}));
   }(this, (function (exports) { 'use strict';
-
-  var F = function F() {
-    return false;
-  };
-
-  exports.F = F;
+  class utils {
+    F() {
+        return false;
+      }
+  }
+  window.utils = new utils();
 })));
