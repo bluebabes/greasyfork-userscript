1d206a8c91c89b447098cfaa8b999043ae4a04b8
bluebabes <a308057848@163.com>
2022-04-07 17:14:53 +0800

    1

--- utils/utils.js
+++ utils/utils.js
@@ -1,31 +1,28 @@
-( function( global, factory ) {
-	"use strict";
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
-		module.exports = global.document ?
-			factory( global, true ) :
-			function( w ) {
-				if ( !w.document ) {
-					throw new Error( "jQuery requires a window with a document" );
-				}
-				return factory( w );
-			};
-	} else {
-		factory( global );
-	}
-
-// Pass this if window is not defined yet
-} )( typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
-
-    "use strict";
-
-    class utils {
-        F() {
-            return false;
+(function (global, factory) {
+  "use strict";
+  if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = global.document
+      ? factory(global, true)
+      : function (w) {
+          if (!w.document) {
+            throw new Error("jQuery requires a window with a document");
           }
-    }
-if ( typeof noGlobal === "undefined" ) {
-    window.utils = new utils();
-}
+          return factory(w);
+        };
+  } else {
+    factory(global);
+  }
 
-return new utils();
-} );
+  // Pass this if window is not defined yet
+})(typeof window !== "undefined" ? window : this, function (window, noGlobal) {
+  "use strict";
+  class utils {
+    F() {
+      return false;
+    }
+  }
+  if (typeof noGlobal === "undefined") {
+    window.utils = new utils();
+  }
+  return new utils();
+});
