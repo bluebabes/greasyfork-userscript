62e36f26f479d187a8c6fed63f2d889c497db92c
bluebabes <a308057848@163.com>
2022-04-07 17:11:02 +0800

    d

--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,35 @@
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-    typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (global = global || self, factory(global.R = {}));
-  }(this, (function (exports) { 'use strict';
-  class utils {
-    F() {
-        return false;
-      }
-  }
-  window.utils = new utils();
-})));
+( function( global, factory ) {
+	"use strict";
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+		module.exports = global.document ?
+			factory( global, true ) :
+			function( w ) {
+				if ( !w.document ) {
+					throw new Error( "jQuery requires a window with a document" );
+				}
+				return factory( w );
+			};
+	} else {
+		factory( global );
+	}
+
+// Pass this if window is not defined yet
+} )( typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
+
+    "use strict";
+
+    class utils {
+        F() {
+            return false;
+          }
+    }
+
+    // Expose jQuery and $ identifiers, even in AMD
+// (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
+// and CommonJS for browser emulators (#13566)
+if ( typeof noGlobal === "undefined" ) {
+    window.utils = new utils();
+}
+
+return new utils();
+} );
