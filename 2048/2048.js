8be5e9dd8b0def326b72d37f2c0aed43592ad1b8
bluebabes <a308057848@163.com>
2022-04-08 15:03:48 +0800

    2048

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         2048帖子高亮图片预览
 // @description  帖子高亮，列表页面直接预览帖子内图片
-// @version      0.0.7
+// @version      0.0.8
 // @author       bluebabes
 // @namespace    hjd2048.com
 // @match        https://hjd2048.com/*
@@ -25,6 +25,7 @@
 
   // debug
   var debug = true;
+  var maxImgCount = 8; // 最多显示图片数量
 
   // 增加菜单
   var menu_ALL = [["menu_disable", "✅ 已启用", "❌ 已禁用", []]];
@@ -85,66 +86,104 @@
 
   // 启用/禁用 (当前网站)
   function menu_disable(type) {
+    let href = document.location.href;
     switch (type) {
       case "check":
-        if (check()) return true;
+        if (disabled()) return true;
         return false;
         break;
       case "add":
-        add();
+        addDisabled();
         break;
       case "del":
-        del();
+        delDisabled();
         break;
     }
 
-    function check() {
+    function disabled() {
       // 存在返回真，不存在返回假
       let websiteList = menu_value("menu_disable"); // 读取网站列表
-      if (websiteList.indexOf(location.host) === -1) return false; // 不存在返回假
+      if (websiteList.indexOf(href) === -1) return false; // 不存在返回假
       return true;
     }
 
-    function add() {
-      if (check()) return;
+    function addDisabled() {
+      if (disabled()) return;
       let websiteList = menu_value("menu_disable"); // 读取网站列表
-      websiteList.push(location.host); // 追加网站域名
+      websiteList.push(href); // 追加网站域名
       GM_setValue("menu_disable", websiteList); // 写入配置
       location.reload(); // 刷新网页
     }
 
-    function del() {
-      if (!check()) return;
+    function delDisabled() {
+      if (!disabled()) return;
       let websiteList = menu_value("menu_disable"), // 读取网站列表
-        index = websiteList.indexOf(location.host);
+        index = websiteList.indexOf(href);
       websiteList.splice(index, 1); // 删除网站域名
       GM_setValue("menu_disable", websiteList); // 写入配置
       location.reload(); // 刷新网页
     }
   }
 
+  // 2048 判断
   var href = document.location.href;
   if (href.indexOf("/2048/") === -1) {
     return;
   }
 
-  // 逻辑处理
-  if (!menu_disable("check")) {
-    // 预览内容图片
+  // 检查是否是列表
+  function isUrlList() {
+    var reg = /fid-[\d]+.html/g;
+    return href.match(reg).length > 0;
+  }
+  // 检查是否是帖子
+  function isUrlDetail() {
+    return !isUrlList;
+  }
+  // 过滤
+  function filterDetailUrl(title) {
+    if (!title) {
+      return true;
+    }
+    var filters = [
+      "來訪者必看的內容",
+      "請各位更換新的文宣",
+      "外站图床推荐",
+      "开放主题",
+      "同城兼职少妇",
+    ];
+    filters.forEach((element) => {
+      if (title.indexOf(element) > 0) {
+        return true;
+      }
+    });
+    return false;
+  }
+
+  // 2048 列表逻辑处理
+  if (isUrlList()) {
+    let origin = document.location.origin;
+    // 列表循环
     $(".tr3").each(function () {
       var that = $(this);
-      var url =
-        document.location.origin + "/2048/" + $(this).find("a").attr("href");
+      var thatA = that.find("a").first();
+      var url = origin + "/2048/" + thatA.attr("href");
 
       var thattd = that.find("td:eq(1)");
       if (href.indexOf("search.php") >= 0) {
         thattd = that.find("th:eq(0)");
       }
 
-      var xmlOn = url.indexOf("read.php") >= 0;
-      if (xmlOn) {
-        utils.Log(debug, ["处理内部帖子图片:", url]);
+      // a的数量异常删除
+      if (thattd.find("a").length > 2) {
+        that.remove();
+      }
 
+      // 处理图片
+      var title = thattd.find("a").first().text();
+      var xmlOn = filterDetailUrl(title);
+      if (!xmlOn && !menu_disable("check")) {
+        utils.Log(debug, ["处理内部帖子图片:",title, url]);
         GM_xmlhttpRequest({
           method: "GET",
           url: url,
@@ -157,17 +196,21 @@
           },
           onload: function (result) {
             var doc = result.responseText;
-            var imgs = $(doc).find(".att_img > img");
+            var imgs = $(doc).find(".tpc_content img"); // $(doc).find(".att_img > img");
 
-            utils.Log(debug, ["获取图片:", imgs.length]);
+            // utils.Log(debug, ["获取图片:", imgs.length]);
 
             for (let i = 0; i < imgs.length; i++) {
+              // 最多图片
+              if (i >= maxImgCount) {
+                break;
+              }
               const element = imgs[i];
               if (i == 0) {
                 thattd.append("<br />");
               }
               var src = element.getAttribute("src");
-              src = src.replace("http://", "https://");
+              src = utils.ImgSrcComplate(src);
 
               thattd.append(
                 "<img object-fit='contain' style='width:200px;' src='" +
@@ -181,31 +224,20 @@
     });
 
     // 高亮回复数大于xx数的帖子
-    var highlightCount = 5;
-    var tr3s = document.querySelectorAll(".tr3");
-    for (var i = 0; i < tr3s.length; i++) {
-      var element = tr3s[i];
-      var td = element.querySelectorAll("td");
+    function highlight() {
+      var highlightCount = 5;
+      var tr3s = document.querySelectorAll(".tr3");
+      for (var i = 0; i < tr3s.length; i++) {
+        var element = tr3s[i];
+        var td = element.querySelectorAll("td");
 
-      // 高亮
-      if (td[3]) {
-        if (td[3].textContent * 1 > highlightCount) {
-          td[1].style.backgroundColor = "#baccd9";
-          td[3].style.backgroundColor = "#baccd9";
+        // 高亮
+        if (td[3]) {
+          if (td[3].textContent * 1 > highlightCount) {
+            td[1].style.backgroundColor = "#baccd9";
+            td[3].style.backgroundColor = "#baccd9";
+          }
         }
-      }
-
-      if (
-        (td[1] && td[1].textContent.indexOf("澳门") > 0) ||
-        (td[1] && td[1].textContent.indexOf("赌场") > 0) ||
-        (td[1] && td[1].textContent.indexOf("上门约啪！") > 0) ||
-        (td[1] && td[1].textContent.indexOf("浏览2048需要注意的点") > 0) ||
-        (td[1] && td[1].textContent.indexOf("免费互约APP") > 0) ||
-        (td[1] && td[1].textContent.indexOf("国产抖阴小视频") > 0) ||
-        (td[1] && td[1].textContent.indexOf("区发贴教程详解") > 0) ||
-        (td[1] && td[1].textContent.indexOf("自售区版规细则及发帖标准") > 0)
-      ) {
-        td[1].parentNode.remove();
       }
     }
   }
