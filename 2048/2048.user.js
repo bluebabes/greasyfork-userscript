16459b2d13776d2b05774acd3aa6125d3c94f2df
bluebabes <a308057848@163.com>
2026-01-14 21:57:49 +0800

    1

--- 2048/2048.user.js
+++ 2048/2048.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         2048帖子高亮图片预览
 // @description  帖子高亮，列表页面直接预览帖子内图片, 更多功能查看readme
-// @version      0.1.1
+// @version      0.1.2
 // @author       bluebabes
 // @namespace    hjd2048.com
 // @match        https://*/*
